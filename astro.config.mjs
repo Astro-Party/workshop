41aa83571a684d6a087f2fb78a49dc625d96405e
Jason Lengstorf <jason@lengstorf.com>
2024-09-14 12:36:08 -0700

    wip: getting there

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  integrations: [db(), react()],
+	output: "hybrid",
+	integrations: [db(), react()],
 });
